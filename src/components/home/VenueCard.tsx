b0232ef30ea6ac5d170a1b72cbc96bea1a7b095f
Slaviša Čovakušić <slavisacovakusic@gmail.com>
2026-04-13 02:23:57 +0200

    fi: make movie/venue cards clickable; tweak grid

--- src/components/home/VenueCard.tsx
+++ src/components/home/VenueCard.tsx
@@ -6,7 +6,7 @@ type VenueCardProps = {
 
 export default function VenueCard({ venue }: VenueCardProps) {
   return (
-    <article className="flex h-98.75 w-full max-w-75.5 flex-col rounded-3xl border border-card-border bg-card-background p-4 shadow-[0px_2px_4px_-2px_rgba(52,64,84,0.08),0px_4px_6px_-1px_rgba(52,64,84,0.08)]">
+    <article className="cursor-pointer flex h-98.75 w-full flex-col rounded-3xl border border-card-border bg-card-background p-4 shadow-[0px_2px_4px_-2px_rgba(52,64,84,0.08),0px_4px_6px_-1px_rgba(52,64,84,0.08)]">
       <img
         src={venue.imageUrl}
         alt={venue.name}
